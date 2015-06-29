efb093115b2040e6e503d985bde024a9a44b2476
na4zagin3 <mrty.ityt.pt@gmail.com>
2015-06-29 18:39:09 +0900

    add build script

---
+++ Setup.hs
@@ -0,0 +1,9 @@
+import Distribution.PackageDescription (HookedBuildInfo)
+import Distribution.Simple
+import Distribution.Simple.Setup (BuildFlags)
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks{preBuild = hook}
+
+hook :: Args -> BuildFlags -> IO HookedBuildInfo
+hook args flags = return (Nothing, [])
